2678a611d96566334f0b9c9f00eb87665c25caf7
mahesh.kolla <mahesh.mk.245@gmail.com>
2024-12-03 22:28:54 +0400

    restructure - move day 2 to day 1, did it by mistake

--- js/day-1/index.test.js
+++ js/day-1/index.test.js
@@ -1,4 +1,4 @@
-import {toArray, makeColumns, sortAll, calculateDifferences, calculateTotalDistance} from "./index";
+import {toArray, makeColumns, sortAll, calculateDifferences, calculateTotalDistance, calculateTotalSimilarityScore, countByGroups} from "./index";
 
 describe("Day 1", () => {
     describe("toArray should convert the input to array ", () => {
@@ -51,5 +51,21 @@ describe("Day 1", () => {
           let actual = calculateTotalDistance(input);
           expect(actual).toEqual(11);
        });
+    });
+
+    describe("countByGroups", () => {
+        test("should give the count of each number in the list", () => {
+            let input = [2, 1, 3, 4, 2, 3, 2, 2];
+            let actual = countByGroups(input);
+            expect(actual).toEqual({1: 1, 2: 4, 3: 2, 4: 1});
+        });
+    });
+
+    describe("calculateTotalSimilarityScore", () => {
+        test("should calculate the total similarity score", () => {
+            let input = "3   4\n" + "4   3\n" + "2   5\n" + "1   3\n" + "3   9\n" + "3   3";
+            let actual = calculateTotalSimilarityScore(input);
+            expect(actual).toEqual(31);
+        });
     });
 });
--- js/day-2/index.test.js
+++ js/day-2/index.test.js
@@ -1,18 +1,6 @@
-import {countByGroups, calculateTotalSimilarityScore} from "./index.js";
-describe("Day 2", () => {
-    describe("countByGroups", () => {
-        test("should give the count of each number in the list", () => {
-            let input = [2, 1, 3, 4, 2, 3, 2, 2];
-            let actual = countByGroups(input);
-            expect(actual).toEqual({1: 1, 2: 4, 3: 2, 4: 1});
-        });
-    });
 
-    describe("calculateTotalSimilarityScore", () => {
-        test("should calculate the total similarity score", () => {
-            let input = "3   4\n" + "4   3\n" + "2   5\n" + "1   3\n" + "3   9\n" + "3   3";
-            let actual = calculateTotalSimilarityScore(input);
-            expect(actual).toEqual(31);
-        });
+describe("Day 2", () => {
+    test("should pass", () => {
+        expect(true).toBeTruthy();
     });
 });
