78d6812f2f990b3055e04de0208d2fbf57423a9e
mahesh.kolla <mahesh.mk.245@gmail.com>
2024-12-03 22:22:05 +0400

    calculate total similarity scode

--- js/day-2/index.js
+++ js/day-2/index.js
@@ -1,3 +1,10 @@
+import {makeColumns, toArray} from "../day-1/index.js";
+
+
+const getOrDefault = (item, defaultValue) => {
+    return item ? item : defaultValue;
+}
+
 const countByGroups = (list) => {
     return list.reduce((result, item) => {
         result[item] = result[item] ? result[item] + 1 : 1;
@@ -5,4 +12,16 @@ const countByGroups = (list) => {
     }, {});
 }
 
-export {countByGroups};
+const calculateTotalSimilarityScore = (input) => {
+    let inputInArray = input.split("\n")
+        .map(line => toArray(line));
+    let columns = makeColumns(inputInArray);
+    let groups = countByGroups(columns[1]);
+    return columns[0].map(item => {
+        let countOnRightColumn = getOrDefault(groups[item], 0);
+        return item * countOnRightColumn;
+    }).reduce((a,b) => a+b, 0);
+
+}
+
+export {countByGroups, calculateTotalSimilarityScore};
