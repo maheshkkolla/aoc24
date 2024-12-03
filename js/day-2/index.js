741478e3ed77107ecddedb0be81466a2aaf71f5d
mahesh.kolla <mahesh.mk.245@gmail.com>
2024-12-03 22:45:56 +0400

    wip - finding if report is safe or not

---
+++ js/day-2/index.js
@@ -0,0 +1,5 @@
+const isReportSafe = (array) => {
+    return false;
+}
+
+export {isReportSafe}
--- js/day-2/index.test.js
+++ js/day-2/index.test.js
@@ -1,6 +1,9 @@
-
+import {isReportSafe} from "./index.js";
 describe("Day 2", () => {
-    test("should pass", () => {
-        expect(true).toBeTruthy();
+    describe("isReportSafe", () => {
+        test("should return false when the numbers are not in either ascending or descending order", () => {
+            expect(isReportSafe([1, 3, 2, 4, 5])).toBeFalsy();
+            expect(isReportSafe([4, 3, 2, 1, 5])).toBeFalsy();
+        });
     });
 });
