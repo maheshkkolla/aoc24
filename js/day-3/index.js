a85ffa5cb1de31821964487811b93279c62206ab
mahesh.kolla <mahesh.mk.245@gmail.com>
2024-12-04 00:12:33 +0400

    day3 - execute all instructions

--- js/day-3/index.js
+++ js/day-3/index.js
@@ -3,4 +3,17 @@ const getMatchedInstructions = (input) => {
     return input.match(regex);
 }
 
-export {getMatchedInstructions};
+const sum = (a, b) => a+b;
+const mul = (a, b) => a*b;
+
+const executeInstruction = (instruction) => {
+    return eval(instruction);
+}
+
+const executeAll = (input) => {
+    return getMatchedInstructions(input)
+        .map(executeInstruction)
+        .reduce(sum, 0);
+}
+
+export {getMatchedInstructions, executeInstruction, executeAll};
--- js/day-3/index.test.js
+++ js/day-3/index.test.js
@@ -1,4 +1,4 @@
-import {getMatchedInstructions} from "./index.js";
+import {getMatchedInstructions, executeInstruction, executeAll} from "./index.js";
 describe("Day 3", () => {
     describe("getMatchedInstructions", () => {
         test("should return all the matched mul instructions", () => {
@@ -6,6 +6,23 @@ describe("Day 3", () => {
             let actual = getMatchedInstructions(input);
 
             expect(actual).toEqual([ "mul(2,4)", "mul(5,5)", "mul(11,8)", "mul(8,5)" ]);
+        });
+    });
+
+    describe("executeInstruction", () => {
+        test("should give the result of the instruction", () => {
+
+            expect(executeInstruction("mul(2,4)")).toEqual(8);
+            expect(executeInstruction("mul(5,5)")).toEqual(25);
+        });
+    });
+
+    describe("executeAll", () => {
+        test("should give the result of the all instructions afte removing the corrupted instructions", () => {
+
+            let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+
+            expect(executeAll(input)).toEqual(161);
         });
     });
 });
