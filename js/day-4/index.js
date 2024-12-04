ccbb12268163ab479a90be2320c51404d35b6e7b
mahesh.kolla <mahesh.mk.245@gmail.com>
2024-12-04 09:54:14 +0400

    get all combinations

--- js/day-4/index.js
+++ js/day-4/index.js
@@ -1,6 +1,9 @@
 const getCombinations = (x, y) => {
     return [
-        [[x, y], [x, y+1], [x, y+2], [x, y+3]]
+        [[x, y], [x, y+1], [x, y+2], [x, y+3]],
+        [[x, y], [x, y-1], [x, y-2], [x, y-3]],
+        [[x, y], [x+1, y], [x+2, y], [x+3, y]],
+        [[x, y], [x-1, y], [x-2, y], [x-3, y]]
     ];
 };
 
--- js/day-4/index.test.js
+++ js/day-4/index.test.js
@@ -5,5 +5,20 @@ describe("Day 4", () => {
             var actual = getCombinations(5,5);
             expect(actual[0]).toEqual([[5,5],[5,6],[5,7],[5,8]]);
         });
+
+        test("should get the left combination", () => {
+            var actual = getCombinations(5,5);
+            expect(actual[1]).toEqual([[5,5],[5,4],[5,3],[5,2]]);
+        });
+
+        test("should get the bottom combination", () => {
+            var actual = getCombinations(5,5);
+            expect(actual[2]).toEqual([[5,5],[6,5],[7,5],[8,5]]);
+        });
+
+        test("should get the top combination", () => {
+            var actual = getCombinations(5,5);
+            expect(actual[3]).toEqual([[5,5],[4,5],[3,5],[2,5]]);
+        });
     });
 });
