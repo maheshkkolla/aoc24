a5b16a268e4c601b6a6eab35c9ec09a7f0d8ae54
mahesh.kolla <mahesh.mk.245@gmail.com>
2024-12-06 10:48:59 +0400

    day 6 - found the location

--- js/day-6/index.js
+++ js/day-6/index.js
@@ -3,4 +3,16 @@ const convertToMatrix = (input) => {
         .map(line => line.split(""));
 }
 
-export {convertToMatrix};
+const findGuard = (matrix) => {
+    let location = [];
+    matrix.forEach((row, index) => {
+        let found = row.indexOf("^");
+        if(found !== -1) {
+            location = [index, found];
+        }
+    });
+
+    return location;
+}
+
+export {convertToMatrix, findGuard};
