9dd861d8bf32db5bf8e09bab5a09daedf61259a5
mahesh.kolla <mahesh.mk.245@gmail.com>
2024-12-07 20:50:04 +0400

    day 7 - initial setup

---
+++ js/day-7/index.test.js
@@ -0,0 +1,18 @@
+import {getTotal} from "index.js";
+describe("Day 7", () => {
+    describe("getTotal", () => {
+        test("should get the total", () => {
+            let input  = "190: 10 19\n" +
+                "3267: 81 40 27\n" +
+                "83: 17 5\n" +
+                "156: 15 6\n" +
+                "7290: 6 8 6 15\n" +
+                "161011: 16 10 13\n" +
+                "192: 17 8 14\n" +
+                "21037: 9 7 18 13\n" +
+                "292: 11 6 16 20";
+
+            expect(getTotal(input)).toEqual(3749);
+        });
+    });
+});
